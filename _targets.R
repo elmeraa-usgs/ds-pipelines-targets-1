44096e0474c9c7979c79db0284f27e9970846195
Azadpour <eazadpour@usgs.gov>
2022-08-17 11:37:26 -0700

    added and modified code, folders and makefiles in _targets.R to build figure_1.png with tar_make

--- _targets.R
+++ _targets.R
@@ -1,35 +1,41 @@
 library(targets)
-source("code.R")
+
+# Source functions
+source("1_fetch/src/fetch.R")
+source("2_process/src/process.R")
+source("3_visualize/src/visualize.R")
+source("3_visualize/src/diagnostics.R")
+
 tar_option_set(packages = c("tidyverse", "sbtools", "whisker"))
 
 list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    download_data(out_filepath = "model_RMSEs.csv"),
+    fetch(file_out = "1_fetch/out/model_RMSEs.csv"),
     format = "file"
   ), 
   # Prepare the data for plotting
   tar_target(
-    eval_data,
-    process_data(in_filepath = model_RMSEs_csv),
+    model_summary_results_csv,
+    process(file_in = model_RMSEs_csv,
+            file_out = "2_process/out/model_summary_results.csv",
+            col = c('#1b9e77', '#d95f02', '#7570b3'),
+            pch = c(21, 22, 23)),
+    format = "file"
   ),
   # Create a plot
   tar_target(
     figure_1_png,
-    make_plot(out_filepath = "figure_1.png", data = eval_data), 
-    format = "file"
-  ),
-  # Save the processed data
-  tar_target(
-    model_summary_results_csv,
-    write_csv(eval_data, file = "model_summary_results.csv"), 
+    visualize(file_in = model_summary_results_csv,
+              file_out = "3_visualize/out/figure_1.png"), 
     format = "file"
   ),
   # Save the model diagnostics
   tar_target(
     model_diagnostic_text_txt,
-    generate_model_diagnostics(out_filepath = "model_diagnostic_text.txt", data = eval_data), 
+    diagnostics(file_in = model_summary_results_csv, 
+      file_out = "3_visualize/out/model_diagnostic_text.txt"), 
     format = "file"
   )
 )
